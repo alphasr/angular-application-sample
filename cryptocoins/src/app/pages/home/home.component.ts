398e14177bcd1f0e7c9017dd41dee98da0790fcf
alphasr <nilaybaranwal@gmail.com>
2022-06-09 18:05:58 +0200

    added styles for ag-grid

--- cryptocoins/src/app/pages/home/home.component.ts
+++ cryptocoins/src/app/pages/home/home.component.ts
@@ -12,14 +12,14 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {}
 
   columnDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
+    { field: 'Name' },
+    { field: 'Symbol' },
+    { field: 'Price' },
   ];
 
   rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
+    { Name: 'Bitcoin', Symbol: 'BTC', Price: 35000 },
+    { Name: 'Ethereum', Symbol: 'ETH', Price: 3200 },
+    { Name: 'Gala', Symbol: 'GAL', Price: 1.5 },
   ];
 }
