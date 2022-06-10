7defb52e093a0abb979dbecabd49c7cdb679a40a
alphasr <nilaybaranwal@gmail.com>
2022-06-10 16:20:57 +0200

    added a list using service from mock data

--- cryptocoins/src/app/pages/home/home.component.ts
+++ cryptocoins/src/app/pages/home/home.component.ts
@@ -9,22 +9,22 @@ import { CryptolistService } from '../../services/cryptolist.service';
 })
 export class HomeComponent implements OnInit {
   constructor(private cryptoListService: CryptolistService) {}
+  rowData: Coin[] = [];
 
   ngOnInit(): void {
     this.getList();
+    this.rowData = this.list;
   }
   list: Coin[] = [];
 
   columnDefs: ColDef[] = [
-    { field: 'name', width: 100, headerName: 'Name' },
-    { field: 'symbol', width: 80, headerName: 'Symbol' },
-    { field: 'price', width: 80, headerName: 'Price' },
+    { field: 'name', width: 100 },
+    { field: 'symbol', width: 80 },
+    { field: 'price', width: 80 },
   ];
   getList(): void {
     this.cryptoListService
       .getList()
       .subscribe((cryptos) => (this.list = cryptos));
   }
-
-  rowData = this.list;
 }
